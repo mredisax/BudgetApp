8a72d567b1d1eec26c08045e9b9dee0df6fc4640
jbauman <mredisax@gmail.com>
2023-10-12 22:55:11 +0200

    init vuejs projects and basic template based on talwind

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <DashBoard />
+  
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import DashBoard from './components/Dashboard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DashBoard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="leading-normal tracking-normal" id="main-body">
+      <div class="flex flex-wrap">
+  
+        <Sidebar />
+        <div class="w-full bg-gray-100 pl-0 lg:pl-64 min-h-screen" :class="sideBarOpen ? 'overlay' : ''" id="main-content">
+          <Navbar />
+  
+          <div class="p-6 bg-gray-100 mb-20">
+            <router-view />
+            <DashboardHome />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+//   import { mapState } from 'vuex'
+  
+  import Sidebar from './Sidebar'
+  import Navbar from './Navbar'
+  import DashboardHome from '../pages/HomePage.vue';
+  
+  export default {
+    name: 'DashBoard',
+    components: {
+      Sidebar,
+      Navbar,
+      DashboardHome
+    }
+  }
+  </script>
